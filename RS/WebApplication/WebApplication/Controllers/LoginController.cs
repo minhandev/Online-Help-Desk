202e89fa933fbcc2a5486b31ab5d566430f9b57c
minhandev <minhan.dev@gmail.com>
2020-07-05 10:30:33 -0700

    The first

---
+++ RS/WebApplication/WebApplication/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("Login")]
+    public class LoginController : Controller
+    {
+        [Route("Index")]
+        [Route("")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword("123", BCrypt.Net.BCrypt.GenerateSalt());
+            return View();
+        }
+
+        [HttpPost]
+        [Route("process")]
+        public IActionResult Process(string username, string password)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ RS/WebApplication/WebApplication/Security/SecurityManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Security
+{
+    public class SecurityManager
+    {
+        public async void SignIn(HttpContext httpContext, Account account)
+        {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(getUserClaims(account), CookieAuthenticationDefaults.AuthenticationScheme);
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+        }
+
+        private IEnumerable<Claim> getUserClaims(Account account)
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, account.Username));
+            claims.Add(new Claim(ClaimTypes.Role, account.Role.Name));
+            return claims;
+        }
+    }
+}
